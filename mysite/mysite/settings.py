5fd95f9c9e72013bd9d593ef81d8309e5f42e79d
Aryan Santosh Sawant <102072945+LaxmansAryan@users.noreply.github.com>
2023-05-20 12:05:38 +0530

    Add files via upload

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -122,3 +122,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+# Email server configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'verydirtyninja69@gmail.com'
+EMAIL_HOST_PASSWORD = '3276 3054'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
